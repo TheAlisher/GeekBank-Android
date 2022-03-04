d075c3b17fa89ca19cd34fb5e91ade66a9b25872
ErlanDZ <omurbaev_erich@mail.ru>
2022-03-04 23:01:17 +0300

    пока закомитил

--- data/src/main/kotlin/com/alish/geekbank/data/repositories/FooRepositoryImpl.kt
+++ data/src/main/kotlin/com/alish/geekbank/data/repositories/FooRepositoryImpl.kt
@@ -1,19 +1,12 @@
 package com.alish.geekbank.data.repositories
 
-import com.alish.geekbank.data.remote.apiservices.FooApiService
-import com.alish.geekbank.data.remote.dtos.toDomain
-import com.alish.geekbank.data.remote.pagingsources.CardsPagingSource
-import com.alish.geekbank.data.repositories.base.BaseRepository
-import com.alish.geekbank.domain.repositories.FooRepository
-import javax.inject.Inject
-
-class CardsRepositoryImpl @Inject constructor(
-    private val service: FooApiService,
-) : BaseRepository(), FooRepository {
-
-    override fun fetchFoo() = doRequest {
-        service.fetchCards().toDomain()
-    }
-
-    fun fetchFooPaging() = doPagingRequest(CardsPagingSource(service))
-}
+//class CardsRepositoryImpl @Inject constructor(
+//    private val service: FooApiService,
+//) : BaseRepository(), FooRepository {
+//
+//    override fun fetchFoo() = doRequest {
+//        service.fetchCards().toDomain()
+//    }
+//
+//    fun fetchFooPaging() = doPagingRequest(CardsPagingSource(service))
+//}
