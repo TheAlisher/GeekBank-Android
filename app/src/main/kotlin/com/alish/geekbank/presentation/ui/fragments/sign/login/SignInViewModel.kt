0b1b7bbe558cf0951b2812ff53d2f5eda85c77a7
ErlanDZ <omurbaev_erich@mail.ru>
2022-03-04 23:12:15 +0300

    добавил  SignInViewModel

---
+++ app/src/main/kotlin/com/alish/geekbank/presentation/ui/fragments/sign/login/SignInViewModel.kt
@@ -0,0 +1,33 @@
+package com.alish.geekbank.presentation.ui.fragments.sign.login
+
+import com.alish.geekbank.domain.usecases.FetchDataUseCase
+import com.alish.geekbank.presentation.base.BaseViewModel
+import com.alish.geekbank.presentation.models.UsersModelUI
+import com.alish.geekbank.presentation.models.toUsersModelUI
+import com.alish.geekbank.presentation.state.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class SignInViewModel @Inject constructor(
+    private val signUseCase: FetchDataUseCase,
+) : BaseViewModel() {
+
+    private val _signState = MutableStateFlow<UIState<List<UsersModelUI?>>>(UIState.Loading())
+    val signState: StateFlow<UIState<List<UsersModelUI?>>> = _signState.asStateFlow()
+
+    init {
+        getData()
+    }
+
+    private fun getData() {
+        signUseCase().collectRequest(_signState) {
+            it.map { users ->
+                users?.toUsersModelUI()
+            }
+        }
+    }
+}
