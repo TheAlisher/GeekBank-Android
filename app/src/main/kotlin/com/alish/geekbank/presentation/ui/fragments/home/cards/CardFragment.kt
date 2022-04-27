46837fc2a13db68133ec71d8f76dad35393b47b5
aiazbekov33 <89203588+aiazbekov33@users.noreply.github.com>
2022-04-27 09:39:33 +0600

    #GTB-14/ Animation in home fragment, card fragment, fab shadow animation update

--- app/src/main/kotlin/com/alish/geekbank/presentation/ui/fragments/home/cards/CardFragment.kt
+++ app/src/main/kotlin/com/alish/geekbank/presentation/ui/fragments/home/cards/CardFragment.kt
@@ -2,11 +2,17 @@ package com.alish.geekbank.presentation.ui.fragments.home.cards
 
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.alish.geekbank.R
 import com.alish.geekbank.data.local.preferences.PreferencesHelper
 import com.alish.geekbank.databinding.FragmentCardBinding
 import com.alish.geekbank.presentation.base.BaseFragment
+import com.alish.geekbank.presentation.models.CardModelUI
+import com.alish.geekbank.presentation.models.HistoryModelUI
+import com.alish.geekbank.presentation.ui.adapters.CardDetailAdapter
+import com.alish.geekbank.presentation.ui.adapters.CardDetailListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -15,16 +21,51 @@ class CardFragment : BaseFragment<CardViewModel, FragmentCardBinding>(R.layout.f
 
     override val viewModel: CardViewModel by viewModels()
     override val binding: FragmentCardBinding by viewBinding(FragmentCardBinding::bind)
+    private val cardDetailAdapter = CardDetailAdapter()
+    private val cardDetailListAdapter = CardDetailListAdapter()
+    private var positionCard = ""
+    val list = ArrayList<CardModelUI>()
 
     @Inject
     lateinit var preferencesHelper: PreferencesHelper
 
     override fun setupListeners() {
-        setupAction()
+//        setupAction()
+        checkPosition()
+    }
+
+    override fun initialize() = with(binding) {
+        listRecyclerCard.adapter = cardDetailAdapter
+        listRecyclerCard.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+    }
+
+    private fun checkPosition() {
+        binding.listRecyclerCard.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val offset: Int = binding.listRecyclerCard.computeHorizontalScrollOffset()
+                var position: Float =
+                    offset.toFloat() / (binding.listRecyclerCard.getChildAt(0).measuredWidth).toFloat()
+                position += 0.5f
+                val postInt: Int = position.toInt()
+                positionCard = cardDetailAdapter.currentList[postInt].cardNumber.toString()
+                val filteredList = ArrayList<HistoryModelUI?>()
+                cardDetailListAdapter.submitList(filteredList.sortedByDescending { data -> data?.dateTime })
+
+
+
+            }
+        })
     }
 
 
-    //    override fun setupRequests() {
+}
+
+
+
+        //    override fun setupRequests() {
 //        viewModel.stateUser.collectUIState {
 //            when (it) {
 //                is UIState.Error -> {}
@@ -41,13 +82,11 @@ class CardFragment : BaseFragment<CardViewModel, FragmentCardBinding>(R.layout.f
 //            }
 //        }
 //    }
-    private fun setupAction() = with(binding) {
-        ivExtraCard.setOnClickListener {
-            findNavController().navigate(CardFragmentDirections.actionCardFragmentToCardDetailFragment())
-        }
-        ivSecond.setOnClickListener {
-            findNavController().navigate(CardFragmentDirections.actionCardFragmentToCardDetailFragment())
-        }
-    }
-
-}
+//    private fun setupAction() = with(binding) {
+//        ivExtraCard.setOnClickListener {
+//            findNavController().navigate(CardFragmentDirections.actionCardFragmentToCardDetailFragment())
+//        }
+//        ivSecond.setOnClickListener {
+//            findNavController().navigate(CardFragmentDirections.actionCardFragmentToCardDetailFragment())
+//        }
+//    }
